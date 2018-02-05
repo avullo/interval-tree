7d7299686106c04a2669c2fb5869bc2a7d1bdccd
Alessandro Vullo <avullo@ebi.ac.uk>
2018-02-05 12:16:36 +0000

    Comparison is based on intervals, not interval data items.

--- itree.h
+++ itree.h
@@ -20,17 +20,16 @@ typedef struct interval interval_t;
 typedef struct itree itree_t;
 typedef struct itreetrav itreetrav_t;
 
-/* User-defined item handling */
-typedef int   (*cmp_f) ( const void *p1, const void *p2 );
+/* User-defined interval data item handling */
 typedef void *(*dup_f) ( void* p );
 typedef void  (*rel_f) ( void* p );
 
 /* Interval tree functions */
-itree_t *itree_new ( cmp_f cmp, dup_f dup, rel_f rel );
+itree_t *itree_new ( dup_f dup, rel_f rel );
 void     itree_delete ( itree_t *tree );
-void    *itree_find ( itree_t *tree, void *data );
-int      itree_insert ( itree_t *tree, void *data );
-int      itree_erase ( itree_t *tree, void *data );
+void    *itree_find ( itree_t *tree, interval_t *interval );
+int      itree_insert ( itree_t *tree, interval_t *interval );
+int      itree_erase ( itree_t *tree, interval_t *interval );
 size_t   itree_size ( itree_t *tree );
 
 /* Tree traversal functions */
