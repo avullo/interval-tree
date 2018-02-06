8f9f9b91e9925b3a5432ed49270edd165b184133
Alessandro Vullo <avullo@ebi.ac.uk>
2018-02-06 12:02:32 +0000

    Basic incomple structure. Support for interval insertion and traversal.

--- itree.h
+++ itree.h
@@ -4,6 +4,11 @@
 /*
   Interval Tree library
 
+  This is an adaptation of the AVL balanced tree C library
+  created by Julienne Walker which can be found here:
+
+  http://www.eternallyconfuzzled.com/Libraries.aspx
+
 */
 #ifdef __cplusplus
 #include <cstddef>
@@ -15,8 +20,12 @@ extern "C" {
 #include <stddef.h>
 #endif
 
+typedef struct interval {
+  float  low, high; /* Interval boundaries, inclusive */
+  void  *data;      /* User-defined content */
+} interval_t;
+
 /* Opaque types */
-typedef struct interval interval_t;
 typedef struct itree itree_t;
 typedef struct itreetrav itreetrav_t;
 
@@ -27,18 +36,18 @@ typedef void  (*rel_f) ( void* p );
 /* Interval tree functions */
 itree_t *itree_new ( dup_f dup, rel_f rel );
 void     itree_delete ( itree_t *tree );
-void    *itree_find ( itree_t *tree, interval_t *interval );
+  // void    *itree_find ( itree_t *tree, interval_t *interval );
 int      itree_insert ( itree_t *tree, interval_t *interval );
-int      itree_erase ( itree_t *tree, interval_t *interval );
+  // int      itree_erase ( itree_t *tree, interval_t *interval );
 size_t   itree_size ( itree_t *tree );
 
 /* Tree traversal functions */
 itreetrav_t *itreetnew ( void );
 void     itreetdelete ( itreetrav_t *trav );
-void    *itreetfirst ( itreetrav_t *trav, itree_t *tree );
-void    *itreetlast ( itreetrav_t *trav, itree_t *tree );
-void    *itreetnext ( itreetrav_t *trav );
-void    *itreetprev ( itreetrav_t *trav );
+interval_t    *itreetfirst ( itreetrav_t *trav, itree_t *tree );
+interval_t    *itreetlast ( itreetrav_t *trav, itree_t *tree );
+interval_t    *itreetnext ( itreetrav_t *trav );
+interval_t    *itreetprev ( itreetrav_t *trav );
 
 #ifdef __cplusplus
 }
