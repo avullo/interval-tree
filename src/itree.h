0b65c1baff1ec35a3133a07a99fa9a2f5513ea38
Alessandro Vullo <avullo@ebi.ac.uk>
2018-02-11 15:07:39 +0000

    License header in all relevant files.

--- src/itree.h
+++ src/itree.h
@@ -1,5 +1,29 @@
-#ifndef INTERVAL_TREE_H
-#define INTERVAL_TREE_H
+/*
+ * Libitree: an interval tree library in C 
+ *
+ * Copyright (C) 2018 Alessandro Vullo 
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
+#ifndef _INTERVAL_TREE_H_
+#define _INTERVAL_TREE_H_
 
 /*
   Interval Tree library
@@ -51,4 +75,4 @@ interval_t  *itreetprev ( itreetrav_t *trav );
 }
 #endif
 
-#endif /* INTERVAL_TREE_H */
+#endif /* _INTERVAL_TREE_H_ */
--- tests/interval.c
+++ tests/interval.c
@@ -1,3 +1,27 @@
+/*
+ * Libitree: an interval tree library in C 
+ *
+ * Copyright (C) 2018 Alessandro Vullo 
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
 #include <check.h>
 #include <stdlib.h>
 #include "interval.h"
@@ -46,13 +70,34 @@ START_TEST(interval_duplication)
   ck_assert_int_eq ( icopy->low, 10 );
   ck_assert_int_eq ( icopy->high, 20 );
   ck_assert_int_eq ( get_data ( icopy->data ), 10 );
-  
-  
+    
   interval_delete( i );
   interval_delete( icopy );
   
 }
 END_TEST
+
+START_TEST(interval_comparison)
+{
+  /* int dummy = 10; */
+  /* interval_t *ints[] = { */
+  /*   interval_new ( 10, 20, &dummy, clone_data, destroy_data ) */
+  /* }; */
+  /* interval_t *icopy = interval_copy ( i ); */
+  
+  /* ck_assert ( icopy ); */
+  /* ck_assert_ptr_ne ( i, icopy ); */
+  /* ck_assert_ptr_ne ( i->data, icopy->data ); */
+  /* ck_assert_int_eq ( icopy->low, 10 ); */
+  /* ck_assert_int_eq ( icopy->high, 20 ); */
+  /* ck_assert_int_eq ( get_data ( icopy->data ), 10 ); */
+    
+  /* interval_delete( i ); */
+  /* interval_delete( icopy ); */
+  
+}
+END_TEST
+
 
 static Suite *interval_suite(void) {
     Suite *s;
@@ -60,14 +105,16 @@ static Suite *interval_suite(void) {
 
     s = suite_create("Interval");
 
-    /* Core test case */
-    tc_core = tcase_create("Core");
-
+    tc_core = tcase_create("Core"); /* Core test case */
+    tc_iops = tcase_create("Operations");
+    
     tcase_add_test(tc_core, interval_creation);
     tcase_add_test(tc_core, interval_duplication);
-
+    tcase_add_test(tc_ops, interval_comparison);
+    
     suite_add_tcase(s, tc_core);
-
+    suite_add_tcase(s, tc_iops);
+    
     return s;
 }
 
